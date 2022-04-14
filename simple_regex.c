1949320f0cc461f623d9a2470717f177c8e58d2f
Muzammiluddin Syed <m38syed@uwaterloo.ca>
2022-04-14 00:57:07 -0400

    Adding state transition function

--- simple_regex.c
+++ simple_regex.c
@@ -4,22 +4,82 @@
 
 //Descripttion of states
 /*
-State 1 - alphanumeric
-State 2 - open bracket
-State 2a - alpha numeric
-State 2b - close bracket
-State 3 - Special character
-State 3.1a - *, repeat last character until no match
-State 
+1. State 1 - alphanumeric
+2. State 2 - open bracket
+3. State 2a - alpha numeric
+4. State 2b - close bracket
+5. State 3 - Special character
+6. State 3.1a - *, repeat last character until no match
+7. State 4 - Done
+8. State 5 - Error
+9. State 6 - alpha special
 */
 
 //this is a preliminary implementation of a finite_automata for use in more complicated regex
 struct finite_automata_state {
+    int state;
     struct finite_automata_state* next_state;
     char* storage;
     int storage_size;
     void (*state_transition)(char* input);
 };
+
+int state_transition(int state, char input){
+    switch(state){
+        case 1:
+            if (input == '*'){
+                return 6;
+            } else if (is_special_character(input)){
+                return 9;
+            } else if (input == '[') {
+                return 2;
+            } else if (input >= 30 || input <= 80) {
+                return 1;
+            } else {
+                return 5;
+            }
+        case 2:
+            if (input == '*'){
+                return 3;
+            } else if (is_special_character(input)){
+                return 5;
+            } else if (input == '[') {
+                return 5;
+            } else if (input >= 30 || input <= 80) {
+                return 3;
+            } else {
+                return 5;
+            }
+        case 3:
+            if (input == '*'){
+                return 5;
+            } else if (is_special_character(input)){
+                return 5;
+            } else if (input == ']') {
+                return 4;
+            } else if (input >= 30 || input <= 80) {
+                return 3;
+            } else {
+                return 5;
+            }
+        case 4:
+            if (input == '*'){
+                return 6;
+            } else if (is_special_character(input)){
+                return 9;
+            } else if (input == '[') {
+                return 2;
+            } else if (input >= 30 || input <= 80) {
+                return 1;
+            } else {
+                return 5;
+            }
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+    }
+}
 
 int is_special_character(char a) {
     switch(a){
